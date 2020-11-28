a4eed130c73c8c358e74f1d8c301914d2fac623c
AnUnawarewolf <royal.victor.leopoldino@gmail.com>
2020-11-28 14:38:49 -0300

    Fixed viewport, made buttons more square, and Save button now adds note to object

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
-const body = document.querySelector(".body-base")
-const header = document.querySelector(".header")
-const sidebar = document.querySelector(".sidebar")
-const main = document.querySelector(".main")
-const textarea = document.querySelector(".main-text")
-const footer = document.querySelector(".footer")
 const lightButton = document.querySelector(".light_theme")
-const buttons = document.querySelectorAll(".btn")
+const newNoteButton = document.querySelector(".new_note")
+const noteDiv = document.querySelector(".note")
+const cancelButton = document.querySelector(".cancel")
+const saveButton = document.querySelector(".save")
+const textarea = document.querySelector(".main-text")
+
+let notesArray = [{ title:"note one", body:"some text" }, 
+                  { title: "note two", body:"some text" }]
+
+saveButton.addEventListener("click", () => {
+    if (textarea.value !== '') {
+        let lines = textarea.value.split("\n")
+        note = {}
+        note.title = lines[0]
+
+        start = 1
+        note.body = ''
+        for (let i = 0; i < lines.length; i++) {
+            note.body += `${lines[start]}\n`
+            start++
+        }
+        notesArray.push(note)
+    console.log(notesArray)
+    }
+})
+
+cancelButton.addEventListener("click", () => {
+    noteDiv.classList.add("hide_note")
+    console.log("cancel button clicked")
+})
+
+newNoteButton.addEventListener("click", () => {
+    if (noteDiv.classList.contains("hide_note")) {
+        noteDiv.classList.remove("hide_note")
+    } else {
+        textarea.value = ''
+    }
+    console.log("new note button clicked")
+})
 
 lightButton.addEventListener("click", () => {
+    const body = document.querySelector(".body-base")
+    const header = document.querySelector(".header")
+    const sidebar = document.querySelector(".sidebar")
+    const main = document.querySelector(".main")
+    const textarea = document.querySelector(".main-text")
+    const footer = document.querySelector(".footer")
+    const buttons = document.querySelectorAll(".btn")
+
     body.classList.toggle("body-light")
     header.classList.toggle("headerfooter-light")
     footer.classList.toggle("headerfooter-light")
